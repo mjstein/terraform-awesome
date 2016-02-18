da9f48b5d0f4d753f7d3aa25e086782ebff17b77
msuddd <msuddd@gmail.com>
2016-02-18 17:58:35 +0000

    it works

--- agent/main.tf
+++ agent/main.tf
@@ -8,6 +8,34 @@ resource "aws_key_pair" "auth" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
+resource "aws_security_group" "blah" {
+  name        = "${var.key_name}"
+  vpc_id      = "${var.vpc_id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # Create a subnet to launch our instances into
 resource "aws_subnet" "subnet" {
@@ -15,3 +43,27 @@ resource "aws_subnet" "subnet" {
   cidr_block              = "${var.cidr_block}"
   map_public_ip_on_launch = true
 }
+
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # The name of our SSH keypair we created above.
+  key_name = "${aws_key_pair.auth.id}"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${aws_security_group.blah.id}"]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = "${aws_subnet.subnet.id}"
+
+}
+
